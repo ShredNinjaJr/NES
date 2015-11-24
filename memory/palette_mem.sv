63553d193ec3965d5c2e9a6385d5ac115d3873de
Vibhakar Vemulapati <Vibhakar Vemulapati>
2015-11-24 11:31:05 -0600

    Added reset values for the palette mem

--- memory/palette_mem.sv
+++ memory/palette_mem.sv
@@ -5,6 +5,7 @@
  */
 
 module palette_mem ( input 			clk,
+							input 			reset,
                      input  [4:0]  addr,
 							input   [7:0] data_in,
 							output logic [7:0] data_out,
@@ -18,9 +19,28 @@ assign data_out = mem_array[addr];
 
 			
 
-always_ff @ (posedge clk)
+always_ff @ (posedge clk or posedge reset)
 begin
-	if(WE)
+	if(reset)
+	begin
+	mem_array[0] <= 8'h22;
+	mem_array[1] <= 8'h29;
+	mem_array[2] <= 8'h1A;
+	mem_array[3] <= 8'h0F;
+	mem_array[4] <= 8'h22;
+	mem_array[5] <= 8'h36;
+	mem_array[6] <= 8'h17;
+	mem_array[7] <= 8'h0F;
+	mem_array[8] <= 8'h22;
+	mem_array[9] <= 8'h30;
+	mem_array[10] <= 8'h21;
+	mem_array[11] <= 8'h0F;
+	mem_array[12] <= 8'h22;
+	mem_array[13] <= 8'h27;
+	mem_array[14] <= 8'h17;
+	mem_array[15] <= 8'h0F;
+	end
+	else if(WE)
 	begin
 		case(addr)
 		/* mirrored palette */
