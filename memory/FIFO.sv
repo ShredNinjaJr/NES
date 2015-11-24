026bdc7d256aa0ade5c01ad053199cde49c9cc8b
Vibhakar Vemulapati <Vibhakar Vemulapati>
2015-11-24 14:01:12 -0600

    Added and tested FIFO module

---
+++ memory/FIFO.sv
@@ -0,0 +1,55 @@
+/* Synchronous FIFO Module used to buffer I/O between dissimlar clocks. */
+/* Can read and write on the same cycle */
+module FIFO #(parameter w = 6, parameter n = 5)
+(
+	input clk, reset, WE, RE,
+	input logic [w-1:0] data_in,
+	output logic [w-1:0] data_out,
+	output logic empty, full
+);
+
+logic [n-1:0] r_addr, w_addr;
+logic [w-1:0] mem_array [2**n-1:0];
+
+logic [n :0] status_cnt;	/* count for number of elements stored */
+
+assign empty = (status_cnt == 0);
+assign full = (status_cnt == (2**n-1));
+
+ 
+always_ff @(posedge clk, posedge reset)
+begin
+
+	if(reset)
+	begin
+		r_addr <= 0;
+		w_addr <= 0;
+		data_out <= 0;
+		status_cnt <= 0;
+	end
+	else
+	begin
+		if(WE)
+		begin: Write
+			if(~full)
+			begin
+				mem_array[w_addr] <= data_in;
+				w_addr <= w_addr + 1;
+				status_cnt <= status_cnt + 1;
+			end
+		end
+		if(RE)
+		begin: Read
+			if(~empty)
+			begin
+				data_out <= mem_array[r_addr];
+				r_addr <= r_addr + 1;
+				status_cnt <= status_cnt - 1;
+			end
+		end
+		
+	end
+end
+
+
+endmodule
---
+++ testbenches/FIFO_testbench.sv
@@ -0,0 +1,80 @@
+module FIFO_testbench();
+
+timeunit 10ns;	// Half clock cycle at 50 MHz
+			// This is the amount of time represented by #1 
+timeprecision 1ns;
+
+// These signals are internal because the processor will be 
+// instantiated as a submodule in testbench.
+logic clk = 0;	
+logic WE, RE;
+logic reset;
+logic [5:0] data_out, data_in;
+logic empty, full;
+integer ErrorCnt  = 0;
+
+
+FIFO fifo_module(.*);
+
+// Toggle the clock
+// #1 means wait for a delay of 1 timeunit
+always begin : CLOCK_GENERATION
+#1 clk = ~clk;
+end
+
+initial begin: CLOCK_INITIALIZATION
+    clk = 1;
+	 reset = 0;
+	 RE = 0;
+	 WE = 0;
+end 
+
+// Testing begins here
+// The initial block is not synthesizable
+// Everything happens sequentially inside an initial block
+// as in a software program
+initial begin: TEST_VECTORS
+
+	reset = 1;
+#2 reset = 0;
+	if(!empty)
+		$display("FIFO NOT EMPTY ON RESET!");
+		
+#4 data_in = 6'h20;
+	WE = 1;
+#2	data_in = 6'h02;
+
+#2 data_in = 6'h34;
+
+#2 data_in = 6'h0F;
+
+#2 data_in = 6'h0F;
+
+#2 data_in = 6'h10;
+
+#2 data_in = 6'h11;
+
+#2 data_in = 6'h12;
+
+#2 data_in = 6'h13;
+
+#2 data_in = 6'h14;
+
+#2 data_in = 6'h15;
+
+#2 data_in = 6'h16;
+
+#2 data_in = 6'h17;
+
+	
+#3 RE = 1;
+	WE = 0;
+
+end
+
+always_ff@(posedge clk)
+begin: display
+
+	$display("data_in = %x, data_out = %x, Read = %d, Write = %d", data_in, data_out, RE, WE);
+end
+endmodule
